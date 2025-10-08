bfe5c388a1754b62a58f33bd62dbe3b5bc4bedf2
liliXsensei <liam.maitre624@gmail.com>
2025-10-08 11:02:00 +0200

    initialisation de trait

--- trait.py
+++ trait.py
@@ -7,7 +7,10 @@ def trait(x1,y1,x2,y2):
         x2, y2 : coordonnées de la fin du trait
     Cette function dessine un trait entre les 2 points transmis en paramètres
     '''
-    pass
+    turtle.penup()
+    turtle.goto(x1,y1)
+    turtle.pendown()
+    turtle.goto(x2,y2)
 
 if __name__ == '__main__':
     trait(0,0,100,100)
