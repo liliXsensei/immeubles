b0f5b02767273322367fb3ed9817a2861ede2c61
liliXsensei <liam.maitre624@gmail.com>
2025-10-08 11:39:57 +0200

    toit1 mauvais

--- toit1.py
+++ toit1.py
@@ -9,6 +9,14 @@ def toit1(x, y_sol, niveau):
     Cette fonction dessine un toit triangulaire noir de base 160 pixels
     et de hauteur centrale 40 pixels
     '''
+
+    turtle.penup()
+    turtle.setx(x)
+    turtle.sety(niveau+(1)*60)
+    turtle.pendown()
+    turtle.forward(80)
+    turtle.right(45)
+    turtle.forward()
     pass
 
 
