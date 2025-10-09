100bc82ed52436dc279f3468db913c4b383ddf41
Guigz577 <clement.cordeau3@gmail.com>
2025-10-09 14:34:54 +0200

    fenetre_balcon terminé

--- fenetre_balcon.py
+++ fenetre_balcon.py
@@ -11,15 +11,37 @@ def fenetre_balcon(x,y):
         Dessine une porte-fenetre avec balcon en 2 temps: la porte fenetre de 30 pixels de large par 50 pixels de hauteur
         puis le balcon
     '''
+
     # porte-fenetre
-
-    pass
-
+    turtle.fillcolor("grey90")
+    turtle.begin_fill()
+    turtle.penup() 
+    turtle.goto(x, y) 
+    turtle.pendown()
+    turtle.forward(15)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(15)
+    turtle.end_fill()
+    
 
     # balcon
-
-
-    pass
+    turtle.penup()
+    turtle.goto(-21, y)
+    turtle.pendown()
+    turtle.width(3)
+    rectangle(0, 0, 42, 25)
+    trait(-15, 0, -15, 25)
+    trait(-9, 0, -9, 25)
+    trait(-3, 0, -3, 25)
+    trait(3, 0, 3, 25)
+    trait(9, 0, 9, 25)
+    trait(15, 0, 15, 25)        
 
 
 
