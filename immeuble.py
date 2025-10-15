ae435383e49e0e66d41f2a445ff2bda74f8898d0
liliXsensei <liam.maitre624@gmail.com>
2025-10-15 11:48:58 +0200

    immeubles pas finit

--- immeuble.py
+++ immeuble.py
@@ -1,41 +1,21 @@
-# module immeuble
-
-from couleur_aleatoire import couleur_aleatoire
-from random import randint
-from rdc import rdc
-from etage import etage
-from toit import toit
-import turtle
-
-def immeuble(x, y_sol):
-    '''
-    Paramètres
-        x : abscisse du centre de l'étage
-        y_sol : ordonnée du sol du la rue
-    Cette fonction dessine un immeuble Le nombre d'étage est compris aléatoirement entre 0 et 4
-    La couleur de la façade et la couleur de la porte sont tirées au hasard
-    '''
-    # Nombre d'étage (aléatoire)
+ 
+    c_facade = couleur_aleatoire()
+    c_porte = couleur_aleatoire()
 
     pass
-
-    #Couleurs des éléments (aléatoire)
-
-    pass
-
     # Dessin du RDC
-
-    pass
+    rdc(x, y_sol, c_facade, c_porte)
 
     # Dessin des étages
 
     pass
+    for niveau in range(1, nb_etages):
+        etage(x, y_sol + 60, c_facade, niveau)
 
     # Dessin du toit
 
     pass
+    toit(x, y_sol, nb_etages)
 
 if __name__ == '__main__':
     immeuble(0,0)
-    # On ferme la fenêtre s'il y a un clique gauche
-    turtle.exitonclick()
