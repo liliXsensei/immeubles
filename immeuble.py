8f65736fbbf32c823e56f6e07b32ed0abdd2e2bf
liliXsensei <liam.maitre624@gmail.com>
2025-10-15 11:52:38 +0200

    immeuble (tjrs pas finit)

--- immeuble.py
+++ immeuble.py
@@ -1,4 +1,33 @@
- 
+# module immeuble
+
+from couleur_aleatoire import couleur_aleatoire
+from random import randint
+from rdc import rdc
+from etage import etage
+from toit import toit
+import turtle
+
+def immeuble(x, y_sol):
+    '''
+    Paramètres
+        x : abscisse du centre de l'étage
+        y_sol : ordonnée du sol du la rue
+    Cette fonction dessine un immeuble Le nombre d'étage est compris aléatoirement entre 0 et 4
+    La couleur de la façade et la couleur de la porte sont tirées au hasard
+    '''
+    # Nombre d'étage (aléatoire)
+
+    pass
+
+    turtle.speed(100)
+
+    # Autoriser les couleurs RVB 0-255
+    turtle.colormode(255)
+
+    pass
+    # Nombre d'étage (aléatoire entre 1 et 5)
+    nb_etages = randint(1, 5)
+
     c_facade = couleur_aleatoire()
     c_porte = couleur_aleatoire()
 
